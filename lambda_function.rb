6067927a105f38bca5f0c1c6dc371d3382d0f2cb
YuriSuzuki0901 <yuri.suzuki@tam-bourine.co.jp>
2021-02-24 14:03:36 +0900

    first commit

---
+++ lambda_function.rb
@@ -0,0 +1,19 @@
+require 'dotenv'
+require 'net/http'
+require "json/add/core"
+require 'jwt'
+require './zoom.rb'
+require './slack.rb'
+require './spread_sheet.rb'
+
+Dotenv.load
+
+def lambda_handler
+    join_url = Zoom.reservation_meeting
+    daily_person = SpreadSheet.fetch_daily_person(ENV['SPREAD_SHEET_URL'])
+    slack = Slack.new(join_url, daily_person)
+    slack.notify_slack
+end
+
+
+pp lambda_handler
